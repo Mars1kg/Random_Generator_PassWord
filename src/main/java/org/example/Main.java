624be711b84e2b45cf254f788df077f2ce5fb7de
sultan <zyldyzbekovsultan02@gmail.com>
2025-02-15 19:45:15 +0600

    Check the project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+
+        input.close();
     }
+
 }

8a3bdcf905f27010a225606c95681a3f95d59398
sultan <zyldyzbekovsultan02@gmail.com>
2025-02-15 21:32:45 +0600

    Начальные этап, прмерная логика, что и как работает

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
+        Scanner input = new Scanner (System.in);
 
-        System.out.println("Check the project");
-        input.close();
+        System.out.println("Enter the length of the password: ");
+        int length = input.nextInt();
+
+        System.out.println("Add UPPERCASE? 1-YES, 0-NO");
+        int useUpper = input.nextInt();
+        String UpperCase = "";
+        if (useUpper == 1){
+            System.out.println("Enter specific UPPERCASE letters: ");
+            UpperCase = input.nextLine();
+        }
+
+        System.out.println("Add lowercase? 1-yes, 0-no");
+        int useLower = input.nextInt();
+        String LowerCase = "";
+        if(useLower == 1){
+            System.out.println("Enter specific lowercase letters: ");
+            LowerCase = input.nextLine();
+        }
+
+        System.out.println("Add numbers? 1-YES, 0-NO");
+        int num = input.nextInt();
+        String numBer = "";
+        if (num == 1){
+            System.out.println("Enter specific numbers: ");
+            numBer = input.nextLine();
+        }
+
+        System.out.println("Add symbols? 1-YES, 0-NO");
+        int sym = input.nextInt();
+        String symBol = "";
+        if (sym == 1){
+            System.out.println("Enter specific symbols: ");
+            symBol = input.nextLine();
+        }
+
+        input.close ();
     }
 
 }

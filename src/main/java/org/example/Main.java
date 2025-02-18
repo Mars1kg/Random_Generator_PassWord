894a94f2512821cc18a4ab2af31953cf44f2154d
sultan <zyldyzbekovsultan02@gmail.com>
2025-02-18 21:48:16 +0600

    Не могу понять как реализовать часть проекта, где программа должна в случайном порядке отбирать из всех введеных пользователем символов, цифр, строчных и заглавных букв. Скоро...

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,17 @@
 package org.example;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
-
         System.out.println("Enter the length of the password: ");
         int length = input.nextInt();
+        input.nextLine();
 
         System.out.println("Add UPPERCASE? 1-YES, 0-NO");
         int useUpper = input.nextInt();
+        input.nextLine();
         String UpperCase = "";
         if (useUpper == 1){
             System.out.println("Enter specific UPPERCASE letters: ");
@@ -18,6 +20,7 @@ public class Main {
 
         System.out.println("Add lowercase? 1-yes, 0-no");
         int useLower = input.nextInt();
+        input.nextLine();
         String LowerCase = "";
         if(useLower == 1){
             System.out.println("Enter specific lowercase letters: ");
@@ -26,6 +29,7 @@ public class Main {
 
         System.out.println("Add numbers? 1-YES, 0-NO");
         int num = input.nextInt();
+        input.nextLine();
         String numBer = "";
         if (num == 1){
             System.out.println("Enter specific numbers: ");
@@ -34,6 +38,7 @@ public class Main {
 
         System.out.println("Add symbols? 1-YES, 0-NO");
         int sym = input.nextInt();
+        input.nextLine();
         String symBol = "";
         if (sym == 1){
             System.out.println("Enter specific symbols: ");
@@ -41,6 +46,8 @@ public class Main {
         }
 
         input.close ();
+
+        String chars = UpperCase + LowerCase + numBer + symBol;
     }
 
 }
